67be953cd45074cbaf56f12a801882c78da5446f
Tony (CTO) <tony@agentpick.co>
2026-03-11 14:36:01 +0000

    fix: sentry sourcemaps config

--- next.config.ts
+++ next.config.ts
@@ -11,7 +11,7 @@ export default withSentryConfig(withNextIntl(nextConfig), {
   project: process.env.SENTRY_PROJECT,
   silent: !process.env.CI,
   widenClientFileUpload: true,
-  hideSourceMaps: true,
+  sourcemaps: { disable: true },
   disableLogger: true,
   automaticVercelMonitors: false,
 });
