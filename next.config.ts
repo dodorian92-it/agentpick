afdec9b992cd334388e3efc6acc9c1c00e1ddb86
Peter <peter@agentpick.co>
2026-03-12 18:13:29 +0000

    fix: disable sentry build wrapper (invalid token blocking CI)

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,8 @@
 import type { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
-import { withSentryConfig } from "@sentry/nextjs";
 
 const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
 
 const nextConfig: NextConfig = {};
 
-export default withSentryConfig(withNextIntl(nextConfig), {
-  org: process.env.SENTRY_ORG,
-  project: process.env.SENTRY_PROJECT,
-  silent: !process.env.CI,
-  widenClientFileUpload: true,
-  sourcemaps: { disable: true },
-  disableLogger: true,
-  automaticVercelMonitors: false,
-});
+export default withNextIntl(nextConfig);
